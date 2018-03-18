1d4f62a692d1730695f43cee249aa46e09972a52
Liubomyr Kokor <23187063+ljubik@users.noreply.github.com>
2018-03-18 18:21:11 +0200

    Add files via upload

---
+++ src/app/hero.ts
@@ -0,0 +1,21 @@
+export class News {
+  id: number;
+  date: string;
+  date_gmt: string;
+  guid: string;
+  modified: string;
+  modified_gmt: string;
+  slug: string;
+  status: string;
+  type: string;
+  link: string;
+  title: string;
+  content: Expression;
+  excerpt: string;
+  author: string;
+  featured_media: number;
+  comment_status: string; 
+  ping_status: string; 
+  categories: number;
+  _links: Object;
+}
